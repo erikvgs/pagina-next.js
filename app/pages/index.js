d2f64ea08f82901b52652a758bbd0f7eb3288070
Erik Vinícius <erik.vgs@outlook.com>
2024-11-02 19:40:23 -0300

    Add files via upload

---
+++ app/pages/index.js
@@ -0,0 +1,15 @@
+jsx
+import Head from 'next/head';
+
+function HomePage() {
+  return (
+    <div>
+      <Head>
+        <title>Home Page</title>
+      </Head>
+      <h1> Olá mundo! </h1>
+    </div>
+  );
+}
+
+export default HomePage;
